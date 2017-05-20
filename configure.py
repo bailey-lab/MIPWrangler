4cd36a749ef226fea464d54b887826694a619b53
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-05-20 16:36:12 -0400

    configure mod;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "MIPWrangler"
-    libs = "elucidator:develop,seekdeep:develop,TwoBit:develop"
+    libs = "elucidator:develop"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
